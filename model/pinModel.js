d0e8aa8aeea2b2a39f3935ad1a31d2faeb1a3b59
Anbu <anbukumar1913@gmail.com>
2025-08-09 16:37:09 +0530

    Initial commit

---
+++ model/pinModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const dataSchema = new mongoose.Schema(
+  {
+    pin:{
+      type: Boolean,
+      default: false,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'users',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+dataSchema.index({ createdAt: 1 })
+const Data = mongoose.model('data', dataSchema)
+
+module.exports = Data
